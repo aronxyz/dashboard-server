61372f471076d294955bab712a8a853018d0945b
aronxyz <aron_xyz@outlook.com>
2025-04-14 14:04:46 +0200

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://dashboard-seven-mu-12.vercel.app/'
+    'https://dashboard-seven-mu-12.vercel.app'
 ]
 
 module.exports = allowedOrigins
